09111b463833be3abacaa3d3ba51036570852ab2
BrianEtemesi <etemecci@gmail.com>
2023-07-14 21:37:12 +0000

    safe divide

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,18 @@
+-- creates a function that divides two numbers
+DELIMITER //
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
+BEGIN
+    DECLARE result FLOAT;
+    
+    IF b = 0 THEN
+        SET result = 0;
+    ELSE
+        SET result = a / b;
+    END IF;
+    
+    RETURN result;
+END //
+
+DELIMITER ;
