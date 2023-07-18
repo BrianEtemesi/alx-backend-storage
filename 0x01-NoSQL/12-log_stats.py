72ae8c92f4fea3cd05ac3e348a4122f2bb436f05
BrianEtemesi <etemecci@gmail.com>
2023-07-18 14:37:33 -0700

    different syntax

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -7,18 +7,17 @@ from pymongo import MongoClient
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
+
     nginx_collection = client.logs.nginx
 
-    c_len = len(list(nginx_collection.find()))
-    print(c_len, "logs\nMethods:")
+    total_logs = nginx_collection.count_documents({})
+    print(f"{total_logs} logs")
 
+    print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        print("\tmethod {}: {}".format(
-            method,
-            len(list(nginx_collection.find({"method": method})))
-        ))
+        count = nginx_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
 
-    print("{} status check".format(
-        len(list(nginx_collection.find({"method": "GET", "path": "/status"})))
-    ))
+    status_check_count = nginx_collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check_count} status check")
