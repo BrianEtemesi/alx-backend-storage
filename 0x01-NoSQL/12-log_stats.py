0e6a641b2a625df41d1889b92a145796be06b8df
BrianEtemesi <etemecci@gmail.com>
2023-07-18 13:54:39 -0700

    log stats

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+script to provides stats about Nginx logs stored in MongoDB
+"""
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient()
+    nginx_collection = client.logs.nginx
+    c_len = len(list(nginx_collection.find()))
+    print(c_len, "logs\nMethods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        print(
+            "\tmethod {}: {}".format(
+                method, len(list(nginx_collection.find({"method": method})))
+                )
+            )
+    print(
+        "{} status check".format(
+            len(list(
+                nginx_collection.find({"method": "GET", "path": "/status"})
+                ))
+            )
+        )
