a4219eb1948be2d9c09499c53a5dcec38bcfe2e2
BrianEtemesi <etemecci@gmail.com>
2023-07-18 14:28:36 -0700

    fix pycode errors

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,26 +1,24 @@
 #!/usr/bin/env python3
 """
-script to provides stats about Nginx logs stored in MongoDB
+script to provide stats about Nginx logs stored in MongoDB
 """
 from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-	client = MongoClient()
-	nginx_collection = client.logs.nginx
+    client = MongoClient()
+    nginx_collection = client.logs.nginx
+
     c_len = len(list(nginx_collection.find()))
     print(c_len, "logs\nMethods:")
+
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        print(
-            "\tmethod {}: {}".format(
-                method, len(list(nginx_collection.find({"method": method})))
-                )
-            )
-    print(
-        "{} status check".format(
-            len(list(
-                nginx_collection.find({"method": "GET", "path": "/status"})
-                ))
-            )
-        )
+        print("\tmethod {}: {}".format(
+            method,
+            len(list(nginx_collection.find({"method": method})))
+        ))
+
+    print("{} status check".format(
+        len(list(nginx_collection.find({"method": "GET", "path": "/status"})))
+    ))
