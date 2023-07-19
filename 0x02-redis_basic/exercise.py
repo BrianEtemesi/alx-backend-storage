faddf0be2b7708cb1ed1933aad152f44fd7e4e97
BrianEtemesi <etemecci@gmail.com>
2023-07-19 19:17:10 +0000

    add test code

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -48,3 +48,16 @@ class Cache:
         get data from redis as an int using given key
         """
         return self.get(key, fn=int)
+
+if __name__ == '__main__':
+	cache = Cache()
+
+	TEST_CASES = {
+	    b"foo": None,
+	    123: int,
+	    "bar": lambda d: d.decode("utf-8")
+	}
+
+	for value, fn in TEST_CASES.items():
+	    key = cache.store(value)
+	    assert cache.get(key, fn=fn) == value
