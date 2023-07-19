e7ecb1f9c0729b1935ea911813f098d3691c12b2
BrianEtemesi <etemecci@gmail.com>
2023-07-19 20:22:52 +0000

    fix all pycode errors

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -90,10 +90,13 @@ class Cache:
         self._redis.mset({key: data})
         return key
 
-    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float, None]:
+    def get(self,
+            key: str,
+            fn: Optional[Callable] = None
+            ) -> Union[str, bytes, int, float, None]:
         """
-        Get the data from Redis using the given key. Optionally, use the callable fn
-        to convert the data back to the desired format.
+        Get the data from Redis using the given key. Optionally, use
+        the callable fn to convert the data back to the desired format.
         """
         data = self._redis.get(key)
 
@@ -116,16 +119,3 @@ class Cache:
         get data from redis as an int using given key
         """
         return self.get(key, fn=int)
-
-if __name__ == '__main__':
-	cache = Cache()
-
-	TEST_CASES = {
-	    b"foo": None,
-	    123: int,
-	    "bar": lambda d: d.decode("utf-8")
-	}
-
-	for value, fn in TEST_CASES.items():
-	    key = cache.store(value)
-	    assert cache.get(key, fn=fn) == value
